79e6efaaa25d912d91ea3d33d1c8df5c289c7b17
kyog02 <kyog02.m@gmail.com>
2019-06-01 13:53:50 +0900

    偶数かどうかで場合分け

---
+++ ABC/ABC109/A.cpp
@@ -0,0 +1,12 @@
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+int main(){
+    int A,B;cin >> A >> B;
+    if(A*B%2 == 0)
+        cout << "No" << endl;
+    else
+        cout << "Yes"<< endl;        
+}
